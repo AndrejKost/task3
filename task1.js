255737ccb8db35b650502690ab6717a237b4f4ba
AndrejKost <vi060480kav@gmail.com>
2022-01-26 23:17:13 +0200

    task 1

--- task1.js
+++ task1.js
@@ -1,1 +1,15 @@
-console.log();
+const arr = [1,2,3];
+let f = function(el, pos) {
+        console.log(`element is ${el}, position index is ${pos}`);
+}
+
+function forEach(arr, func) {
+    if (Array.isArray(arr) && typeof func === 'function') {
+        for (let i = 0; i < arr.length; i++) {
+            //let item = arr[i];
+            f(arr[i], i);
+        }
+    }
+}
+
+forEach(arr, f);
